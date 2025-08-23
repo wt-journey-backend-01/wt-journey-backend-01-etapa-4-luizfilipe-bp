c3438bb4bb298c3b302982f02848e2d83c08ec6b
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2025-08-23 19:15:36 -0300

    feat: add middleware de autenticacao

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const ApiError = require('../utils/ApiError');
+
+async function authenticateToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        throw new ApiError(401, 'Token não fornecido', {
+            token: 'O token de autenticação é necessário',
+        });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            throw new ApiError(401, 'Token inválido ou expirado');
+        }
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = { authenticateToken };
