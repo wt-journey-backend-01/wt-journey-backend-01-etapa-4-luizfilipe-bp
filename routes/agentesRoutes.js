1c7231ddd30cbd4c3a00db08331d3ea79b7337e4
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2025-08-23 19:16:42 -0300

    feat: protecao nas rotas de agentes e casos

--- routes/agentesRoutes.js
+++ routes/agentesRoutes.js
@@ -4,18 +4,20 @@ const agentesController = require('../controllers/agentesController');
 const validateIDParam = require('../utils/validateIDParam');
 const { validateSchema } = require('../utils/validateSchema');
 const { postAgenteSchema, putAgenteSchema, patchAgenteSchema } = require('../utils/agentesSchema');
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
-router.get('/:id/casos', validateIDParam, agentesController.getCasosByAgente);
-router.get('/', agentesController.getAllAgentes);
-router.get('/:id', validateIDParam, agentesController.getAgenteById);
-router.post('/', validateSchema(postAgenteSchema), agentesController.postAgente);
-router.put('/:id', validateIDParam, validateSchema(putAgenteSchema), agentesController.putAgente);
+router.get('/:id/casos', authenticateToken, validateIDParam, agentesController.getCasosByAgente);
+router.get('/', authenticateToken, agentesController.getAllAgentes);
+router.get('/:id', authenticateToken, validateIDParam, agentesController.getAgenteById);
+router.post('/', authenticateToken, validateSchema(postAgenteSchema), agentesController.postAgente);
+router.put('/:id', authenticateToken, validateIDParam, validateSchema(putAgenteSchema), agentesController.putAgente);
 router.patch(
     '/:id',
+    authenticateToken,
     validateIDParam,
     validateSchema(patchAgenteSchema),
     agentesController.patchAgente
 );
-router.delete('/:id', validateIDParam, agentesController.deleteAgente);
+router.delete('/:id', authenticateToken, validateIDParam, agentesController.deleteAgente);
 
 module.exports = router;
--- routes/casosRoutes.js
+++ routes/casosRoutes.js
@@ -4,14 +4,27 @@ const casosController = require('../controllers/casosController');
 const validateIDParam = require('../utils/validateIDParam');
 const { validateSchema } = require('../utils/validateSchema');
 const { postCasoSchema, putCasoSchema, patchCasoSchema } = require('../utils/casosSchema');
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
-router.get('/search', casosController.searchCasos);
-router.get('/:id/agente', validateIDParam, casosController.getAgenteByCaso);
-router.get('/', casosController.getAllCasos);
-router.get('/:id', validateIDParam, casosController.getCasoById);
-router.post('/', validateSchema(postCasoSchema), casosController.postCaso);
-router.put('/:id', validateIDParam, validateSchema(putCasoSchema), casosController.updateCaso);
-router.patch('/:id', validateIDParam, validateSchema(patchCasoSchema), casosController.patchCaso);
-router.delete('/:id', validateIDParam, casosController.deleteCaso);
+router.get('/search', authenticateToken, casosController.searchCasos);
+router.get('/:id/agente', authenticateToken, validateIDParam, casosController.getAgenteByCaso);
+router.get('/', authenticateToken, casosController.getAllCasos);
+router.get('/:id', authenticateToken, validateIDParam, casosController.getCasoById);
+router.post('/', authenticateToken, validateSchema(postCasoSchema), casosController.postCaso);
+router.put(
+    '/:id',
+    authenticateToken,
+    validateIDParam,
+    validateSchema(putCasoSchema),
+    casosController.updateCaso
+);
+router.patch(
+    '/:id',
+    authenticateToken,
+    validateIDParam,
+    validateSchema(patchCasoSchema),
+    casosController.patchCaso
+);
+router.delete('/:id', authenticateToken, validateIDParam, casosController.deleteCaso);
 
 module.exports = router;
