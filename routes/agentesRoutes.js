f21788ddb5ba290d8e08037010924e780133e17b
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2025-09-02 14:37:08 -0300

    fix: voltando com validacao id

--- routes/agentesRoutes.js
+++ routes/agentesRoutes.js
@@ -8,7 +8,7 @@ const { authenticateToken } = require('../middlewares/authMiddleware');
 
 router.get('/:id/casos', authenticateToken, validateIDParam, agentesController.getCasosByAgente);
 router.get('/', authenticateToken, agentesController.getAllAgentes);
-router.get('/:id', authenticateToken, agentesController.getAgenteById);
+router.get('/:id', authenticateToken, validateIDParam, agentesController.getAgenteById);
 router.post('/', authenticateToken, validateSchema(postAgenteSchema), agentesController.postAgente);
 router.put(
     '/:id',
